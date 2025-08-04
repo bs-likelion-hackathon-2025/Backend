3008e4f2f5cd883fab88ab8b16e103565b95e7e7
김양현 <gim-yanghyeon@gim-yanghyeons-MacBook-Pro.local>
2025-08-04 19:28:56 +0900

    feature/roulette : 룰렛 컨트롤러:

--- src/main/java/com/example/Cheonan/Configuration/SwaggerConfiguration.java
+++ src/main/java/com/example/Cheonan/Configuration/SwaggerConfiguration.java
@@ -1,4 +1,19 @@
 package com.example.Cheonan.Configuration;
 
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfiguration {
+
+    @Bean
+    public OpenAPI apiInfo() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("pitstop")
+                        .description("pitstop 프로젝트의 Swagger 문서입니다.")
+                        .version("1.0.0"));
+    }
 }
--- src/main/java/com/example/Cheonan/Configuration/WebConfiguration.java
+++ src/main/java/com/example/Cheonan/Configuration/WebConfiguration.java
@@ -1,4 +1,26 @@
 package com.example.Cheonan.Configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
 public class WebConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173","https://pit--stop.vercel.app/") // React 주소
+                        .allowedHeaders("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowCredentials(true);
+            }
+        };
+    }
+
+
 }
